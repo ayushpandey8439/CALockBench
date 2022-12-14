1a5e1a0d81e938247b51b8de08e626166fde15a3
Ayush Pandey <ayush.pandey@lip6.fr>
2022-12-14 19:43:43 +0100

    Pass inherited params from main script to subscripts

--- scripts/script_coarse.sh
+++ scripts/script_coarse.sh
@@ -1,19 +1,14 @@
 echo >./benchmarkResults/coarse.txt
 echo "Thread count 1"
 for((i=0;i< $ITERATIONS_PER_THREAD;i++));do ./sb7_lock -s $SIZE -l c -n 1 -w $LOAD_TYPE -d $DURATION| grep -i "throughput";done >>./benchmarkResults/coarse.txt
-
 echo "Thread count 2"
 for((i=0;i< $ITERATIONS_PER_THREAD;i++));do ./sb7_lock -s $SIZE -l c -n 2 -w $LOAD_TYPE -d $DURATION| grep -i "throughput";done >>./benchmarkResults/coarse.txt
-
 echo "Thread count 4"
 for((i=0;i< $ITERATIONS_PER_THREAD;i++));do ./sb7_lock -s $SIZE -l c -n 4 -w $LOAD_TYPE -d $DURATION| grep -i "throughput";done >>./benchmarkResults/coarse.txt
-
 echo "Thread count 8"
 for((i=0;i< $ITERATIONS_PER_THREAD;i++));do ./sb7_lock -s $SIZE -l c -n 8 -w $LOAD_TYPE -d $DURATION| grep -i "throughput";done >>./benchmarkResults/coarse.txt
-
 echo "Thread count 16"
 for((i=0;i< $ITERATIONS_PER_THREAD;i++));do ./sb7_lock -s $SIZE -l c -n 16 -w $LOAD_TYPE -d $DURATION| grep -i "throughput";done >>./benchmarkResults/coarse.txt
-
 echo "Thread count 32"
 for((i=0;i< $ITERATIONS_PER_THREAD;i++));do ./sb7_lock -s $SIZE -l c -n 32 -w $LOAD_TYPE -d $DURATION| grep -i "throughput";done >>./benchmarkResults/coarse.txt
 
