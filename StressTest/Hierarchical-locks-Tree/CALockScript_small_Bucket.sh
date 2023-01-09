83ba387d419519dc1e62fb64a8620ba6efc49bf1
Ayush Pandey <ayush.pandey@lip6.fr>
2023-01-09 17:09:14 +0100

    Fix Scripts for Benchmarks

--- StressTest/Hierarchical-locks-Tree/CALockScript_small_Bucket.sh
+++ StressTest/Hierarchical-locks-Tree/CALockScript_small_Bucket.sh
@@ -1,7 +1,7 @@
-echo >>./output/CALock_Bucket.txt
+echo >./output/CALock_Bucket.txt
 for j in {1..8};do
 for i in {0..4};do
-./a.out 10000 16 7 4 $((2**$i)) m >>./output/CALock_Bucket.txt;
+./a.out 1000 8 7 4 $((2**$i)) m >>./output/CALock_Bucket.txt;
 done
 echo >>./output/CALock_Bucket.txt
 done
--- StressTest/Hierarchical-locks-Tree/ILscript_small_Bucket.sh
+++ StressTest/Hierarchical-locks-Tree/ILscript_small_Bucket.sh
@@ -1,7 +1,7 @@
-echo >>./output/Intention_Bucket.txt
+echo >./output/Intention_Bucket.txt
 for j in {1..8};do
 for i in {0..4};do
-./a.out 10000 16 4 4 $((2**$i)) m >>./output/Intention_Bucket.txt
+./a.out 1000 8 4 4 $((2**$i)) m >>./output/Intention_Bucket.txt
 done
 echo >>./output/Intention_Bucket.txt
 done
