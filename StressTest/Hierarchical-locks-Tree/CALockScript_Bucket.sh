62fdd2892b806543a8ad54893d9d45ccb8985f83
Ayush PANDEY <pandey@newmail.lip6.fr>
2023-01-09 14:34:00 +0100

    Add Fix scripts for bucketed benchmarks

--- StressTest/Hierarchical-locks-Tree/CALockScript_Bucket.sh
+++ StressTest/Hierarchical-locks-Tree/CALockScript_Bucket.sh
@@ -1,7 +1,7 @@
 echo >>./output/CALock_Bucket.txt
 for j in {1..8};do
 for i in {0..4};do
-./a.out 1000000 16 7 8 $((2**$i)) m >>./output/CALock_Bucket.txt;
+./a.out 1000000 16 7 8 $((2**$j)) m >>./output/CALock_Bucket.txt;
 done
 echo >>./output/CALock_Bucket.txt
 done
