78d98760c601e8a88a63186a7434ab20e60e68e2
Ayush Pandey <ayushpandey8439@gmail.com>
2023-01-10 15:31:32 +0100

    Unify run script for performance benchmark

--- Performance Tests/benchmark.sh
+++ Performance Tests/benchmark.sh
@@ -4,13 +4,13 @@ make all
 
 echo Step 1 of 6: Read Dominated load without modifications
 ./scripts/read.sh
-#echo Step 2 of 6: Read Dominated load with modifications
-#./scripts/readModifications.sh
-#echo Step 3 of 6: Write Dominated load without modifications
-#./scripts/write.sh
-#echo Step 4 of 6: Write Dominated load with modifications
-#./scripts/writeModifications.sh
-#echo Step 5 of 6: Balanced load without modifications
-#./scripts/balanced.sh
-#echo Step 6 of 6: Balanced load with modifications
-#./scripts/writeModifications.sh
+echo Step 2 of 6: Read Dominated load with modifications
+./scripts/readModifications.sh
+echo Step 3 of 6: Write Dominated load without modifications
+./scripts/write.sh
+echo Step 4 of 6: Write Dominated load with modifications
+./scripts/writeModifications.sh
+echo Step 5 of 6: Balanced load without modifications
+./scripts/balanced.sh
+echo Step 6 of 6: Balanced load with modifications
+./scripts/writeModifications.sh
