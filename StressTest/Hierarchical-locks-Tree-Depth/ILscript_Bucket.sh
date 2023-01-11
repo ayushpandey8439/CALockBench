45771f833734cd1ebdce7b80f4ddf7c43ef32ba0
Ayush Pandey <ayush.pandey@lip6.fr>
2023-01-11 18:42:10 +0100

    Save stable run results

--- StressTest/Hierarchical-locks-Tree-Depth/ILscript_Bucket.sh
+++
@@ -1,7 +0,0 @@
-echo >./output/Intention_Bucket.txt
-for j in {0..7};do
-for i in {0..4};do
-./a.out 1000000 8 4 8 $((2**$j)) m >>./output/Intention_Bucket.txt
-done
-echo >>./output/Intention_Bucket.txt
-done
