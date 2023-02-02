8c2dbc4ae6e74ca53527b16684f89fabe3438615
Ayush Pandey <ayush.pandey@lip6.fr>
2023-02-02 11:34:25 +0100

    Save state for stress tests.

--- StressTest/Hierarchical-locks-Tree/CALockScript_Bucket.sh
+++ StressTest/Hierarchical-locks-Tree/CALockScript_Bucket.sh
@@ -1,7 +1,7 @@
 echo >./output/CALock_Bucket.txt
 for j in {0..6};do
 for i in {0..4};do
-./a.out 1000000 8 7 4 $((2**$j)) m false>>./output/CALock_Bucket.txt;
+./a.out 1000000 16 7 4 $((2**$j)) m false>>./output/CALock_Bucket.txt;
 done
 echo >>./output/CALock_Bucket.txt
 done
--- StressTest/Hierarchical-locks-Tree/ILscript_Bucket.sh
+++ StressTest/Hierarchical-locks-Tree/ILscript_Bucket.sh
@@ -1,7 +1,7 @@
 echo >./output/Intention_Bucket.txt
 for j in {0..6};do
 for i in {0..4};do
-./a.out 1000000 4 4 4 $((2**$j)) m false>>./output/Intention_Bucket.txt
+./a.out 1000000 16 4 4 $((2**$j)) m false>>./output/Intention_Bucket.txt
 done
 echo >>./output/Intention_Bucket.txt
 done
