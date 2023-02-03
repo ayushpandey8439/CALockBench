19eeff3e91d6513a2e0702af3fdb3596a931d4d8
Ayush Pandey <ayush.pandey@lip6.fr>
2023-02-04 00:25:30 +0100

    Add shared mutexes instead of spin locks

--- Performance Tests/src/lockPool.h
+++ Performance Tests/src/lockPool.h
@@ -46,6 +46,7 @@ public:
     mutex lockPoolLock;
     lockObject* locks[SIZE];
     shared_mutex threadMutexes[SIZE];
+    condition_variable_any threadConditions[SIZE];
     long int Gseq;
     std::chrono::duration<long double, std::nano> idleness[SIZE];
     std::chrono::duration<long double, std::nano> modificationTimeCA;
@@ -58,6 +59,24 @@ public:
         }
     }
 
+    void waitResolve(int i, int threadID){
+        /// Threads are blocked until the condition is true;
+        abc:
+        std::shared_lock<shared_mutex> ul (threadMutexes[i]);
+        bool waitstatus = threadConditions[i].wait_for(ul,1ms, [this, i, threadID] {
+            return (locks[i] == nullptr ||
+                    /// Read vs Read is not a conflict
+                    (locks[threadID]->mode == 0 && locks[i]->mode == 0) ||
+                    /// The hierarchies do not overlap
+                    (locks[threadID]->Id != locks[i]->Id &&
+                     !locks[threadID]->criticalAncestors->contains(locks[i]->Id)) ||
+                    /// If all else fails, then at least it should be my turn.
+                    locks[threadID]->Oseq <= locks[i]->Oseq);
+        });
+        if(!waitstatus) {
+            goto abc;
+        }
+    }
 
     bool acquireLock(lockObject * reqObj, int threadID) {
         auto t1 = std::chrono::high_resolution_clock::now();
@@ -69,22 +88,24 @@ public:
         for(int i=0;i< SIZE; i++){
             /// A thread won't run into conflict with itself.
            if(locks[i] != nullptr) {
-                /// Spin waiting on the condition.
-                auto l = locks[i];
-                if (l!= nullptr &&
-                 /// If a read lock is requested for an object that is read locked, only then allow it.
-                 (reqObj->mode == 1 || (reqObj->mode==0 && l->mode == 1)) &&
-                 /// Someone else has requested a lock on my LSCA before me.
-                 (reqObj->Id == l->Id || reqObj->criticalAncestors->contains(l->Id) || l->criticalAncestors->contains(reqObj->Id)) &&
-                 /// It isn't my turn to take the lock
-                 (reqObj->Oseq > l->Oseq)) {
-                    //cout<<i<<endl;
-                    threadMutexes[i].lock_shared();
-                    threadMutexes[i].unlock_shared();
+               waitResolve(i, threadID);
 
-                    //if(processor_Count<parameters.getThreadNum()) this_thread::yield();
-                    //l=locks[i];
-                }
+                /// Spin waiting on the condition.
+//                auto l = locks[i];
+//                if (l!= nullptr &&
+//                 /// If a read lock is requested for an object that is read locked, only then allow it.
+//                 (reqObj->mode == 1 || (reqObj->mode==0 && l->mode == 1)) &&
+//                 /// Someone else has requested a lock on my LSCA before me.
+//                 (reqObj->Id == l->Id || reqObj->criticalAncestors->contains(l->Id) || l->criticalAncestors->contains(reqObj->Id)) &&
+//                 /// It isn't my turn to take the lock
+//                 (reqObj->Oseq > l->Oseq)) {
+//                    //cout<<i<<endl;
+//                    threadMutexes[i].lock_shared();
+//                    threadMutexes[i].unlock_shared();
+//
+//                    //if(processor_Count<parameters.getThreadNum()) this_thread::yield();
+//                    //l=locks[i];
+//                }
             }
         }
         auto t2 = std::chrono::high_resolution_clock::now();
